7a9270b39a1580226c72d08adef5612bd0ebb571
Ben <ong.ben@gmail.com>
2025-08-08 17:32:52 -0700

    Check in new router and controller

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,68 +1,18 @@
 const express = require('express');
+const logger = require('./logger');
+const peopleRouter = require('./routes/people');
 
 const app = express();
 
-const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
-    next();
-};
-
+//-- Static assets
 app.use(logger);
+//-- Parse FORM data
+app.use(express.urlencoded({ extended: false }));
+//-- Parse JSON
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.send('Home page');
-});
+app.use('/api/v1/people', peopleRouter);    // This must be after the express.json()
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const logger = require('./logger');
-// const authorize = require('./authorize');
-
-// const app = express();
-
-// // request => middleware => response
-// app.use([authorize, logger]);
-
-// app.get('/', (req, res) => {
-//     res.send('Home page');
-// });
-
-// app.get('/about', (req, res) => {
-//     res.send('About page');
-// });
-
-// app.get('/api/products', (req, res) => {
-//     res.send('Products Page');
-// });
-
-// app.get('/api/items', (req, res) => {
-//     console.log(req.user);
-//     res.send('Items page');
-// });
-
-// app.get('*', (req, res) => {
-//     res.status(404).send('404 Resource not found');
-// });
-
-// app.listen(3000, () => {
-//     console.log('Listening on port 3000...');
-// });
