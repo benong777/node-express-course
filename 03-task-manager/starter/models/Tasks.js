ee4ba14f297ddc97914c7dbe2f72aa46ecb5a516
Ben <ong.ben@gmail.com>
2025-08-25 14:16:54 -0700

    Added data validation, error handling and asyncWrapper

--- 03-task-manager/starter/models/Tasks.js
+++ 03-task-manager/starter/models/Tasks.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 
 //-- Schema sets up structure (data/type) for all the documents in the collection
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, 'Must provide a name'],
+    trim: true,     // Remove white spaces
+    maxlength: [20, 'Name cannot be more than 20 characters'],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  }
 });
 
 //-- Model wrapper provides an interface to the DB (for crud operations)
