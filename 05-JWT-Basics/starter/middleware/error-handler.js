d697d447468c113fff5c316e47a2d8636ade511b
Ben <ong.ben@gmail.com>
2025-09-19 22:32:30 -0700

    Changed res.send to res.json, which is what the frontend is expecting

--- 05-JWT-Basics/starter/middleware/error-handler.js
+++ 05-JWT-Basics/starter/middleware/error-handler.js
@@ -7,7 +7,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   }
   return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .send('Something went wrong try again later');
+    .json({ err })
 }
 
 module.exports = errorHandlerMiddleware
